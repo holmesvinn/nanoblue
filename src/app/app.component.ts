e6ee357676c92a966b92da241124f690c311297b
holmesvinn <holmesvinn@outlook.com>
2019-07-03 21:43:28 +0530

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nanoblue';
+
+  updateDatabase(value){
+    console.log(value); 
+  } 
+ 
+  ngOnInit(){
+    let bluetoothNavigator: any = window.navigator;
+    if(bluetoothNavigator && bluetoothNavigator.bluetooth){
+      bluetoothNavigator.bluetooth.requestDevice({acceptAllDevices:true}).then((device) => {
+        console.log(device);
+      });
+
+    }
+     
+  }
 }
